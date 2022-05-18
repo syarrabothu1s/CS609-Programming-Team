98a3c6486776e65d237796da6a0f11d471d2c5d9
syarrabothu1s <105734529+syarrabothu1s@users.noreply.github.com>
2022-05-17 23:52:12 -0500

    paddle block updation
    paddle block

--- Programming/game/__init__.py
+++ Programming/game/__init__.py
@@ -1,6 +1,8 @@
 import pygame
 import sys
 from game.constants import Constants
+from game.player import Player
+
 
 class Game:
     def __init__(self):
@@ -9,10 +11,20 @@ class Game:
         self.clock = pygame.time.Clock()
 
         self.font = pygame.font.Font('assets/Kenney_Future_Square.ttf',16)
-
         self.bg_color = pygame.Color("black")
 
+        self.player = Player()
+
+        self.all_sprites = pygame.sprite.Group()
+        self.all_sprites.add(self.player)
+
+
     def handle_events(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.player.move_left()
+        if keys[pygame.K_RIGHT]:
+            self.player.move_right()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -20,7 +32,12 @@ class Game:
         pass
 
     def update(self):
-        pass
+        self.all_sprites.update()
+        pygame.display.update()
+        self.clock.tick(120)
+
+
 
     def draw(self):
         self.screen.fill(self.bg_color)
+        self.all_sprites.draw(self.screen)
